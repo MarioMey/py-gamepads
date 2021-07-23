62f9d52b71500bf6a4538d2aa855ff8758c20db2
Mario Mey <mariomey@gmail.com>
2021-07-23 13:05:29 -0300

    Update bt_gamepads.py

--- bt_gamepads.py
+++ bt_gamepads.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+""" py-gamepads
+This code is for connecting two bluetooth gamepads.
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Mario Mey"
+__contact__ = "mariomey@gmail.com"
+__credits__ = ["Sam, from Core Electronics"]
+__date__ = "2021/07/08"
+__deprecated__ = False
+__license__ = "GPLv3"
+__maintainer__ = "developer"
+__status__ = "Production"
+__version__ = "0.2"
 
 import json
 import time, argparse
