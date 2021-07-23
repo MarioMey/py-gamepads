d7abe2f6cda974ccee2cf5836d6759de2103c9b7
Mario Mey <mariomey@gmail.com>
2021-07-23 13:04:32 -0300

    Update bt_gamepads.py

--- bt_gamepads.py
+++ bt_gamepads.py
@@ -1,33 +1,11 @@
 #!/usr/bin/env python3
-""" py-gamepads
-This code is for connecting two bluetooth gamepads.
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation, either version 3 of the License, or (at your option) any later
-version.
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
-"""
-
-__author__ = "Mario Mey"
-__contact__ = "mariomey@gmail.com"
-__credits__ = ["Sam, from Core Electronics"]
-__date__ = "2021/07/08"
-__deprecated__ = False
-__license__ = "GPLv3"
-__maintainer__ = "developer"
-__status__ = "Production"
-__version__ = "0.2"
-
-import time, argparse, subprocess
+import json
+import time, argparse
 try:
-	from obs_api import client, consola, c2, c3, c, thread
+	from obs_api import client, consola, c2, c3, c4, c, thread
 except:
-	from obs_api_no_obs import client, consola, c2, c3, c, thread
+	from obs_api_no_obs import client, consola, c2, c3, c4, c, thread
 	import sys
 
 from evdev import InputDevice, ecodes
@@ -35,89 +13,83 @@ from selectors import DefaultSelector, EVENT_READ
 
 class Bt:
 	def __init__(self):
-		self.but = [307, 308, 305, 304, 315]
-		self.gamepad1, self.gamepad2 = None, None
+		self.buttons = [307, 308, 305, 304, 315, 310, 311, 312, 313]
 		self.selector = DefaultSelector()
-		self.devices_list = list()
-		self.devices_dict = dict()
+		self.devices = dict()
 		self.bt_on = True
 
-	def bt_send_hat(self, path, que, val):
-		client.send_message('/bt', [int(path[-2:]), que, val])
-		c2(f'/bt, {int(path[-2:])}, {que}, {val}')
-		if val == 0: self.devices_dict[path] = 'c'
-		else:        self.devices_dict[path] = que
+	def bt_send_hat(self, dev, path, que, val):
+		client.send_message('/bt', [dev, que, val])
+		c2(f'/bt, {dev}, {path[-2:]}, {que}, {val}')
+		
+		for k,v in self.devices.items():
+			if v['path'] == path:
+				dev = k
+		
+		if val == 0:
+			self.devices[dev]['hat'] = 'c'
+		else:
+			self.devices[dev]['hat'] = que
+	
+	def bt_send(self, dev, path, que, val):
+		#region bt_send(self, path, que, val):
+		client.send_message('/bt', [dev, que, val])
+		c2(f'/bt, {dev}, {path[-2:]}, {que}, {val}')
+		#endregion
 
-	def bt_send(self, path, que, val):
-		client.send_message('/bt', [int(path[-2:]), que, val])
-		c2(f'/bt, {int(path[-2:])}, {que}, {val}')
+	def connect(self, num):
 
-	def reconnect(self):
-		device1 = '13:57:90:05:0E:31'
-		device2 = '13:6E:0E:07:0E:31'
-		ps1 = subprocess.Popen(['bluetoothctl', 'info', device1], stdout=subprocess.PIPE)
-		ps2 = subprocess.Popen(['bluetoothctl', 'info', device2], stdout=subprocess.PIPE)
-		stdout1 = subprocess.check_output(['grep', 'Connected'], stdin=ps1.stdout).decode("utf-8")
-		stdout2 = subprocess.check_output(['grep', 'Connected'], stdin=ps2.stdout).decode("utf-8")
-		if 'No' in stdout1 or 'no' in stdout1:
-			subprocess.Popen(['bluetoothctl', 'connect', device1])
-			c3(f'bluetoothctl connect {device1}')
-		if 'No' in stdout2 or 'no' in stdout2:
-			subprocess.Popen(['bluetoothctl', 'connect', device2])
-			c3(f'bluetoothctl connect {device2}')
+		for dev in range(15, 30):
 
-	def is_none(self, num, dev):
-		gamepad = f'gamepad{num}'
-		device = f'/dev/input/event{dev}'
-		try:
-			vars(self)[gamepad] = InputDevice(device)
-			try:     self.selector.unregister(vars(self)[gamepad])
-			except:  c3(f'Todavía no registrado {device}', c.azul)
+			if dev in [val['event'] for val in self.devices.values()]:
+				c4(f'dev {dev} ya está en la lista')
+				continue
+
 			try:
-				     self.selector.register(vars(self)[gamepad], EVENT_READ)
-				     c3(f'Registrado {device}', c.cian)
-			except:  c3(f'{device} already registred', c.cian)
+				self.devices[num]['InputDevice'] = InputDevice(f'/dev/input/event{dev}')
+				self.selector.register(self.devices[num]['InputDevice'], EVENT_READ)
+				self.devices[num]['path'] = f'/dev/input/event{dev}'
+				self.devices[num]['event'] = dev
+				self.devices[num]['uniq'] = self.devices[num]['InputDevice'].uniq
+				self.devices[num]['phys'] = self.devices[num]['InputDevice'].phys
+				
+				c3(f'Registrado /dev/input/event{dev}', c.debug)
+				client.send_message(f'bt_init{num}', dev)
+				time.sleep(1)
+				break
 
-		except OSError as e:
-			c3(f'No está conectado {device}')
-
-			# Probando device + 1
-			dev += 1
-			device = f'/dev/input/event{dev}'
-			try:
-				vars(self)[gamepad] = InputDevice(device)
-				try:     self.selector.unregister(vars(self)[gamepad])
-				except:  c3(f'Todavía no registrado {device}', c.azul)
-				try:
-						self.selector.register(vars(self)[gamepad], EVENT_READ)
-						c3(f'Registrado {device}', c.cian)
-				except:  c3(f'{device} already registred', c.cian)
 			except OSError as e:
-				c3(f'Ni tampoco...     {device}')
+				# c3(f'No está conectado /dev/input/event{dev}')
+				pass
 
 	def check_devices(self):
+		
 		while self.bt_on:
-			# Si no están cargados, los intenta cargar y registrarlos en selector
-			if self.gamepad1 is None:
-				self.is_none(1, self.devices_list[0])
-			if self.gamepad2 is None:
-				self.is_none(2, self.devices_list[1])
+			for num, val in self.devices.items():
+				if val['InputDevice'] == None:
+					self.connect(num)
+			# break
 			time.sleep(1)
 
-	def input_bt(self, gp1, gp2):
-		self.devices_list = [gp1, gp2]
-		self.devices_dict= {f'/dev/input/event{gp1}':'c',
-					        f'/dev/input/event{gp2}':'c'}
-		
-		client.send_message('/bt_init', [gp1, gp2])
+	def input_bt(self, cantidad):#, gp1, gp2):
+		c4(f'input_bt()', c.violeta)
 
+		for num in range(cantidad):
+			self.devices[num] = {
+				'InputDevice': None,
+				'event': None,
+				'path': '',
+				'hat': 'c',
+				'uniq':None,
+				'phys':None
+				}
+		
 		thread(self.check_devices)
 		
-		time.sleep(2)
 		while self.bt_on:
 			
-			# Si ninguno de los dos está cargado, vuelve a intentar conectarlos
-			if self.gamepad1 is None and self.gamepad2 is None:
+			# Si ninguno de los dos está cargado, ni se fija
+			if all(item == None for item in [val for val in self.devices.values()]):
 				c3('No está conectado ninguno')
 				time.sleep(1)
 				continue
@@ -126,70 +98,105 @@ class Bt:
 			for key, mask in self.selector.select():
 				device = key.fileobj
 				path   = key.fileobj.path
-				
-				# Intenta leer en device. Si salta error...
-				try:
-					for event in device.read():
-						et, ec, ev = event.type, event.code, event.value
-						if et == ecodes.EV_ABS:
-							# Analogo
-							if ec == 1: self.bt_send(path, 'h', -ev)
-							if ec == 0: self.bt_send(path, 'v', -ev)
-							if   ec == 16 and ev == -1: self.bt_send_hat(path, 't', 1)
-							elif ec == 16 and ev ==  1: self.bt_send_hat(path, 'b', 1)
-							elif ec == 17 and ev == -1: self.bt_send_hat(path, 'r', 1)
-							elif ec == 17 and ev ==  1: self.bt_send_hat(path, 'l', 1)
-							if   ec == 1  and ev ==  0: self.bt_send_hat(path, 'r', 0)
-							if   ec == 1  and ev ==  0: self.bt_send_hat(path, 'l', 0)
-							if   ec == 0  and ev ==  0: self.bt_send_hat(path, 't', 0)
-							if   ec == 0  and ev ==  0: self.bt_send_hat(path, 'b', 0)
-							
-						if et == ecodes.EV_KEY:
-							if   ec == self.but[0]: self.bt_send(path, 0, ev)
-							elif ec == self.but[1]: self.bt_send(path, 1, ev)
-							elif ec == self.but[2]: self.bt_send(path, 2, ev)
-							elif ec == self.but[3]: self.bt_send(path, 3, ev)
-							elif ec == self.but[4]: self.bt_send(path, 4, ev)
-				
-				# ... es porque el gamepad se apagó. Lo cierra y lo desregistra de selector
-				except OSError as e:
-					device.close()
-					c3('input_bt() - Except - Se apagó un gamepad')
-					
-					if path[-2:] == '16':
-						c3(f'¿Se apagó /dev/input/event{self.devices_list[0]}? Desregistrándolo...')
-						if self.gamepad1 != None:
-							self.selector.unregister(self.gamepad1)
-							self.gamepad1 = None
-					
-					if path[-2:] == '20':
-						c3(f'¿Se apagó /dev/input/event{self.devices_list[1]}? Desregistrándolo...')
-						if self.gamepad2 != None:
-							self.selector.unregister(self.gamepad2)
-							self.gamepad2 = None
-			
-			# c4('input_bt() Fin de WHILE')
+				for k,v in self.devices.items():
+					if v['path'] == path:
+						dev = k
 
+
+				# Intenta leer en device.
+				try:
+					self.read_device(dev, path, device)
+
+				# Si salta error es porque el gamepad se apagó.
+				except:
+					event = int(path[-2:])
+					for k, v in self.devices.items():
+						if v['event'] == event: num = k
+
+					c3(f'¿Se apagó gamepad{num} {path}? Desregistrándolo...', c.error)
+					self.selector.unregister(self.devices[num]['InputDevice'])
+					device.close()
+					self.devices[num] = {
+						'event': None,
+						'path': '',
+						'hat': 'c',
+						'InputDevice': None,
+						'uniq':None,
+						'phys':None
+						}
+
+	def read_device(self, dev, path, device):
+
+		for event in device.read():
+			et, ec, ev = event.type, event.code, event.value
+			if et == ecodes.EV_ABS:
+				# Analogo
+				if ec == 1:                 self.bt_send(    dev, path, 'h', -ev)
+				if ec == 0:                 self.bt_send(    dev, path, 'v', -ev)
+				if   ec == 16 and ev == -1: self.bt_send_hat(dev, path, 't', 1)
+				elif ec == 16 and ev ==  1: self.bt_send_hat(dev, path, 'b', 1)
+				elif ec == 17 and ev == -1: self.bt_send_hat(dev, path, 'r', 1)
+				elif ec == 17 and ev ==  1: self.bt_send_hat(dev, path, 'l', 1)
+				if   ec == 1  and ev ==  0: self.bt_send_hat(dev, path, 'r', 0)
+				if   ec == 1  and ev ==  0: self.bt_send_hat(dev, path, 'l', 0)
+				if   ec == 0  and ev ==  0: self.bt_send_hat(dev, path, 't', 0)
+				if   ec == 0  and ev ==  0: self.bt_send_hat(dev, path, 'b', 0)
+				
+			if et == ecodes.EV_KEY:
+				if   ec == self.buttons[0]: self.bt_send(dev, path,   0, ev)
+				elif ec == self.buttons[1]: self.bt_send(dev, path,   1, ev)
+				elif ec == self.buttons[2]: self.bt_send(dev, path,   2, ev)
+				elif ec == self.buttons[3]: self.bt_send(dev, path,   3, ev)
+				elif ec == self.buttons[4]: self.bt_send(dev, path,   4, ev)
+				elif ec == self.buttons[5]: self.bt_send(dev, path,   5, ev)
+				elif ec == self.buttons[6]: self.bt_send(dev, path,   6, ev)
+				elif ec == self.buttons[7]: self.bt_send(dev, path,   7, ev)
+				elif ec == self.buttons[8]: self.bt_send(dev, path,   8, ev)
+				
+def write_file():
+	while True:
+		with open("/home/mario/meytv/obs-scripts/write_file.watch", "w") as outfile:
+			new_dict = {}
+			for k,v in bt.devices.items():
+				new_dict[k] = {
+					# 'InputDevice': None,
+					'event': v['event'],
+					'path':  v['path'],
+					'hat':   v['hat'],
+					'uniq':  v['uniq'],
+					'phys':  v['phys']
+					}
+			json.dump(new_dict, outfile, default=str, sort_keys=True, indent=2, ensure_ascii=False)
+		time.sleep(0.25) # 16fps
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--gp1', type=int, default=16,help='Gamepad 1')
-	parser.add_argument('--gp2', type=int, default=20,help='Gamepad 2')
+	parser.add_argument('--c', type=int, default=2,help='Cantidad')
 	args = parser.parse_args()
-	
-	bt = Bt()
 
 	try:
 		consola(f'"R": reconnect()', c.naranja)
 		consola(f'"Q": quit', c.naranja)
 
-		thread(bt.input_bt, [args.gp1, args.gp2])
+		bt = Bt()
+		thread(bt.input_bt, [args.c])
+		thread(write_file)
 
 		while True:
 			tecla = input()
-			if tecla == 'r':
-				bt.reconnect()
-			elif tecla == 'q':
+
+			# Quit	
+			if tecla == 'q':
 				sys.exit()
+
+			# Test
+			elif tecla == 't':
+				consola(f'bt.devices {bt.devices}')
+				# for n, i in enumerate(bt.devices_list):
+				# 	try:
+				# 		consola(vars(bt)[f'gamepad{n}'])
+				# 	except:
+				# 		consola(f'No encuentra {n} {i}')
+
 	except KeyboardInterrupt:
 		print(' Bye')
