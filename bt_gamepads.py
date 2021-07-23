5c63380466b0bbdb67bfec4a0a430b73937d45d7
Mario Mey <mariomey@gmail.com>
2021-07-23 13:10:56 -0300

    Update bt_gamepads.py

--- bt_gamepads.py
+++ bt_gamepads.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ py-gamepads
-This code is for connecting two bluetooth gamepads.
+This code is for connecting two ore more bluetooth gamepads.
 This program is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
 Foundation, either version 3 of the License, or (at your option) any later
